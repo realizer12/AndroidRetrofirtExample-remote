f8815e61e0c5ca9607f654f11d18f79505246a39
LeeDonghun <dlwldlwldus123@gmail.com>
2020-05-02 12:22:05 +0900

    네번째 커밋

---
+++ app/src/main/java/com/example/leedonghun/androidretrofitexample/Activity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.leedonghun.androidretrofitexample.Activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.leedonghun.androidretrofitexample.R
+
+/**
+ * AndroidRetrofitExample
+ * Class: splashactivity.
+ * Created by leedonghun.
+ * Created On 2020-04-28.
+ * Description:
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashactiivity)
+
+
+    }//oncreatea() 끝
+
+
+}
